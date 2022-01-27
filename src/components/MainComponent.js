f41457b50b7b7075468a357dd7ac970b3469fb76
Paul Jacob <paul.jacob@icloud.com>
2022-01-26 20:29:07 -0500

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
-import { PARTNERS }  from '../shared/partners';
+import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
 class Main extends Component {
     constructor(props) {
@@ -28,13 +28,22 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home 
+                <Home
                     campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
                     promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
                     partner={this.state.partners.filter(partner => partner.featured)[0]}
                 />
             );
-        };
+        }
+
+        const CampsiteWithId = ({ match }) => {
+            return (
+                <CampsiteInfo
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        }
 
         return (
             <div>
@@ -42,6 +51,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
